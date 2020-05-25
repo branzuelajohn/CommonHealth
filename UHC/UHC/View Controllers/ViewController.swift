6a38c2f4e93bf88941b37157015a35d58521894e
Liu Xiaowen <liuxiaowen@Lius-MacBook-Air.local>
2020-05-25 21:46:33 +0800

    set up storyboards

---
+++ UHC/UHC/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UHC
+//
+//  Created by Liu Xiaowen on 25/5/20.
+//  Copyright © 2020 CommonHealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
