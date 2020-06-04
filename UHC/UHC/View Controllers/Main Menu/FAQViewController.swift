b70b84a687be42178722f2298bc9a62110d607b7
Liu Xiaowen <liuxiaowen@Lius-MacBook-Air.local>
2020-06-04 10:39:59 +0800

    created new view controllers for menu items

---
+++ UHC/UHC/View Controllers/Main Menu/FAQViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FAQViewController.swift
+//  UHC
+//
+//  Created by Liu Xiaowen on 31/5/20.
+//  Copyright © 2020 CommonHealth. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
