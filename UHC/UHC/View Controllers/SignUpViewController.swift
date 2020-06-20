0d95fd1f023e5df607e25a2148dd2dedb73b2573
John Alec <branzuelajohn@gmail.com>
2020-06-21 01:47:33 +0800

    Changed Sign Up Page to include NRIC, confirm password fields, etc

--- UHC/UHC/View Controllers/SignUpViewController.swift
+++ UHC/UHC/View Controllers/SignUpViewController.swift
@@ -16,49 +16,70 @@ import ProgressHUD
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var titleTextLabel: UILabel!
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var confirmEmailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    @IBOutlet weak var dobTextField: UITextField!
+    @IBOutlet weak var NRICTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var signInButton: UIButton!
     
     
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setUpUI()
+        datePickerStyler()
     }
     
     func setUpUI() {
         
         errorLabel.alpha = 0
         Utilities.styleLabel(titleTextLabel)
-        Utilities.styleTextField(usernameTextField)
+        Utilities.styleTextField(nameTextField)
         Utilities.styleTextField(emailTextField)
         Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(signUpButton)
+        Utilities.styleTextField(confirmEmailTextField)
+        Utilities.styleTextField(NRICTextField)
+        Utilities.styleTextField(mobileNumberTextField)
+        Utilities.styleTextField(confirmPasswordTextField)
+        Utilities.styleTextField(dobTextField)
         
     }
     
     //Check the fields and validate that the data is correct. If everything is correct, this method returns nil. Otherwise, it returns the error message.
     func validateFields() -> String? {
         // Check that all fields are filled in
-        if usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+        if nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
             emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            confirmEmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            NRICTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            mobileNumberTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            confirmPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
             return "Please fill in all fields"
         }
         
         // Check if the password is secure
         let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let confirmedpw = confirmPasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
         if Utilities.isPasswordValid(cleanedPassword) == false {
             // Password isn't secure enough
             return "Please make sure your password is at least 8 characters, contains a special character and a number"
         }
         
+        if confirmedpw != cleanedPassword {
+            // Passwords are not the same
+            return "Please make sure your passwords are the same."
+        }
         return nil
     }
     
@@ -75,9 +96,12 @@ class SignUpViewController: UIViewController {
         } else {
         
             // Create cleaned versions of the data
-            let username = usernameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let name = nameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
             let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let NRIC = NRICTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let mobileNum = mobileNumberTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
             let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let dob = dobTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
             
             // Create the User
             Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
@@ -93,7 +117,7 @@ class SignUpViewController: UIViewController {
                     ProgressHUD.dismiss()
                     let db = Firestore.firestore()
                     
-                    db.collection("users").addDocument(data: ["username" : username, "uid": result!.user.uid]) { (error) in
+                    db.collection("users").addDocument(data: ["username" : name, "NRIC" : NRIC, "Mobile Number" : mobileNum, "Date Of Birth" : dob , "uid" : result!.user.uid]) { (error) in
                         
                         
                         if error != nil {
@@ -121,6 +145,35 @@ class SignUpViewController: UIViewController {
          view.window?.rootViewController = homeViewController
          view.window?.makeKeyAndVisible()
      }
+    
+    
+    func datePickerStyler() {
+        // Create a tool bar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // Make the bar "Done" button
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneButton], animated: true)
+        // Assign the Toolbar
+        dobTextField.inputAccessoryView = toolbar
+        
+        // Assign the datePicker to text field
+        dobTextField.inputView = datePicker
+        
+        // Datepicker mode
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc func donePressed() {
+        //formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        dobTextField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
     
     
     
