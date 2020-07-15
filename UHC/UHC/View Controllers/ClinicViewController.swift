67156a236a4d2e68ded545c90c176b365ebdcf53
John Alec <branzuelajohn@gmail.com>
2020-07-15 16:02:13 +0800

    Added google maps

--- UHC/UHC/View Controllers/ClinicViewController.swift
+++ UHC/UHC/View Controllers/ClinicViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import os.log
 import Firebase
+import GoogleMaps
 
 class ClinicViewController: UIViewController {
 
+    @IBOutlet weak var mapView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var QButton: UIButton!
@@ -26,6 +28,7 @@ class ClinicViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupMap()
         
         if let clinic = clinic {
             navigationItem.title = clinic.name
@@ -37,7 +40,7 @@ class ClinicViewController: UIViewController {
             if let document = document, document.exists {
                 self.QNumber = document.get("Next Qnumber") as! Int
                 let current = document.get("Currently Serving") as! Int
-                self.currentLabel.text = "Number of people currently in queue: " + (String)(self.QNumber - current - 1)
+                 self.currentLabel.text = "Number of people currently in queue: " + (String)(self.QNumber - current - 1)
                 let location = document.get("Location") as! String
                 let time = document.get("Operating Hours") as! String
                 self.descriptionTextView.text = "Location: \(location) \n\n" + "Operating Hours: \n\((time).replacingOccurrences(of: "\n", with: "\n"))"
@@ -45,6 +48,7 @@ class ClinicViewController: UIViewController {
                 print("Document does not exist")
             }
         }
+        
         Utilities.styleFilledButton(self.QButton)
     }
     
@@ -69,5 +73,38 @@ class ClinicViewController: UIViewController {
         print(QNumber)
         db.collection("clinics").document(clinicName).collection("queue").addDocument(data: ["Name": userName!,"NRIC": userNRIC!, "Qnumber": QNumber])
         db.collection("clinics").document(clinicName).setData(["Next Qnumber": QNumber+1], merge: true)
+    }
+    
+    func setupMap() {
+        GMSServices.provideAPIKey("AIzaSyD67SclbwC6a6cqvy3sE7E_cLBWK2hDgdU")
+        
+        db.collection("clinics").document(clinic!.name).getDocument{ (document, error) in
+        if let document = document, document.exists {
+            let geopoint = document.get("GoogleMapLocation") as! GeoPoint
+            let lat = geopoint.latitude
+            let long = geopoint.longitude
+            let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 15)
+            //Indicating map frame bounds
+            let gmap = GMSMapView.map(withFrame: self.mapView.bounds, camera: camera)
+            gmap.isMyLocationEnabled = true
+            
+            //Enable control gestures
+            gmap.settings.zoomGestures = true
+            gmap.settings.scrollGestures = true
+            gmap.settings.rotateGestures = true
+            gmap.settings.tiltGestures = true
+            
+            //My Location
+            gmap.settings.myLocationButton = true
+            
+            self.mapView.addSubview(gmap)
+            
+            let currentLocation = CLLocationCoordinate2DMake(lat, long)
+            let marker = GMSMarker(position: currentLocation)
+            let name = document.get("name") as! String
+            marker.title = name
+            marker.map = gmap
+            }
+        }
     }
 }
